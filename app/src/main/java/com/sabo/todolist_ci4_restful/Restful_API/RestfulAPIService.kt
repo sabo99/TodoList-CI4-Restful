881d428a0011655716df600586eb0f7991a1af33
sabo99 <alvin.set99@gmail.com>
2021-08-29 00:36:14 +0700

    Change BASE_URL

--- app/src/main/java/com/sabo/todolist_ci4_restful/Restful_API/RestfulAPIService.kt
+++ app/src/main/java/com/sabo/todolist_ci4_restful/Restful_API/RestfulAPIService.kt
@@ -7,9 +7,10 @@ class RestfulAPIService {
     companion object {
         private var retrofit: Retrofit? = null
 
-//        private const val URL = "http://192.168.1.6/Restful-API/todolist-ci4-restful/public/"
-        private const val URL = "https://sabohao.000webhostapp.com/"
-//        private const val URL = "http://sabo.infinityfreeapp.com/"
+//        private const val URL = "http://todolist-ci4-restfulapi.net/"
+        private const val URL = "http://192.168.1.8/ci4todolist-restfulapi/public/"
+//        private const val URL = "https://sabohao.000webhostapp.com/"
+//        private const val URL = "https://sabo.infinityfreeapp.com/"
         const val IMG_TODO_URL = "${URL}assets/uploads/todoList/"
         const val AVATAR_TODO_URL = "${URL}assets/uploads/users/"
 
